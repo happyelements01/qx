3386383ea8350de5b9414f32502a0bb729768f21
Carl <houyang126@126.com>
2023-11-09 10:11:45 +0800

    Update xunlei.js

--- xunlei.js
+++ xunlei.js
@@ -2,23 +2,19 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj.vip_info = [{
-    "register": "19700101",
+    "register": "20200101",
     "autodeduct": "0",
-    "daily": "-10",
-    "expire": "0",
+    "daily": "10",
+    "expire": "20290101",
     "grow": "20",
     "is_vip": "1",
     "last_pay": "0",
-    "level": "0",
+    "level": "8",
     "pay_id": "0",
     "remind": "0",
     "is_year": "1",
     "user_vas": "2",
-    "vas_type": "5",
-    "vip_icon": {
-        "general": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_a/vip_level1_deactivate.png",
-        "small": "https://xluser-ssl.xunlei.com/v1/file/icon/level/vip/deactivate_b/vip_lever1_deactivate.png"
-    }
+    "vas_type": "5"
 }]
 
 body = JSON.stringify(obj);
